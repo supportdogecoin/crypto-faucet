436e0b1657321e985f8ec281e61a9813b9e9658f
Crypto Faucet Admin <supportdogecoin@gmail.com>
2026-05-06 17:44:14 -0300

    Add Next.js and Vercel configuration files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
-}
+  experimental: {
+    serverComponentsExternalPackages: ['firebase/auth', 'firebase/firestore', 'firebase/analytics']
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
